4131b72ed58236d113a8c9197deac68c2122eca6
wand-CE <wanderson14117@gmail.com>
2024-10-12 16:39:28 -0400

    removendo erro no nome do app base

--- base_app/apps.py
+++ base_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GerenciadorBaseAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gerenciador_base_app'
+    name = 'base_app'
